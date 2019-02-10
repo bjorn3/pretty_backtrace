42597ac79967b7a4821a4461abdfe6ff76886c49
bjorn3 <bjorn3@users.noreply.github.com>
2019-02-10 14:13:24 +0100

    Add frame source view

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
+#[macro_use]
+extern crate rental;
+
 use std::panic::{PanicInfo, set_hook, take_hook};
+use std::path::PathBuf;
+
+mod syntax_highlight;
 
 lazy_static::lazy_static! {
     static ref HOOK: Box<for<'a> Fn(&'a PanicInfo) + Sync + Send + 'static> = {
@@ -23,7 +29,7 @@ fn the_hook(info: &PanicInfo) {
         }
     };
     let location = info.location().unwrap();
-    eprintln!("thread '{}' panicked at '{}', {}", name, msg, location);
+    eprintln!("thread '{}' \x1b[91m\x1b[1mpanicked\x1b[0m at '{}', {}", name, msg, location);
     eprintln!("stack backtrace:");
 
     with_context(|context| {
@@ -35,7 +41,7 @@ fn the_hook(info: &PanicInfo) {
                 let function_name = frame.function.map(|n|n.demangle().unwrap().to_string()).unwrap_or("<??>".to_string());
 
                 if first_frame {
-                    eprintln!("{:>4}: {:<80}  ({:p})", i, function_name, stack_frame.ip());
+                    eprintln!("\x1b[2m{:>4}:\x1b[0m {:<80}  \x1b[2m({:p})\x1b[0m", i, function_name, stack_frame.ip());
                 } else {
                     eprintln!("      {}", function_name);
                 }
@@ -46,6 +52,11 @@ fn the_hook(info: &PanicInfo) {
                     eprintln!("             at <no debuginfo>");
                 }
                 first_frame = false;
+            }
+
+            if i % 100 == 99 {
+                eprintln!("Backtrace is very big, sleeping 1s...");
+                ::std::thread::sleep_ms(1000);
             }
         }
     });
@@ -87,8 +98,29 @@ fn print_location(location: addr2line::Location) {
         "<???>".to_string()
     };
     match (location.line, location.column) {
-        (Some(line), Some(column)) => eprintln!("             at {}:{}:{}", file, line, column),
-        (Some(line), None) => eprintln!("             at {}:{}", file, line),
-        (None, _) => eprintln!("             at {}", file),
+        (Some(line), Some(column)) => eprintln!("      --> {}:{}:{}", file, line, column),
+        (Some(line), None) => eprintln!("      --> {}:{}", file, line),
+        (None, _) => eprintln!("      --> {}", file),
+    }
+
+    if !file.starts_with("<") {
+        if let Some(line) = location.line {
+            syntax_highlight::with_highlighted_source(PathBuf::from(file.clone()), move |highlighted| {
+                for (line_num, line_str) in highlighted.iter().enumerate().map(|(line_num, line_str)|(line_num as u64 + 1, line_str)) {
+                //if let Ok(file_content) = std::fs::read_to_string(&file) {
+                //    for (line_num, line_str) in file_content.lines().enumerate().map(|(line_num, line_str)|(line_num as u64 + 1, line_str)) {
+                        if line_num < line - 2 || line_num > line + 2 {
+                            continue;
+                        }
+
+                        let line_marker = if line_num as u64 == line { "\x1b[91m>\x1b[0m" } else { " \x1b[2m" };
+                        eprintln!("{} {:>6} | {}", line_marker, line_num, syntax_highlight::as_24_bit_terminal_escaped(line_str, false));
+                        if line_num as u64 == line {
+                            eprintln!("         | {:width$}\x1b[91m^\x1b[0m", " ", width=location.column.unwrap() as usize);
+                        }
+                //    }
+                }
+            });
+        }
     }
 }
---
+++ src/syntax_highlight.rs
@@ -0,0 +1,78 @@
+use std::collections::HashMap;
+use std::sync::Mutex;
+use std::path::PathBuf;
+
+use syntect::easy::HighlightLines;
+use syntect::highlighting::{Color, Style, ThemeSet};
+use syntect::parsing::SyntaxSet;
+
+use self::rent_highlight_cache::*;
+
+
+lazy_static::lazy_static! {
+    static ref SYNTAX_SET: SyntaxSet = SyntaxSet::load_defaults_nonewlines();
+    static ref THEME_SET: ThemeSet = ThemeSet::load_defaults();
+    static ref CACHED_HIGHLIGHTED_FILES: Mutex<HashMap<PathBuf, Option<HighlightCacheEntry>>> = {
+        Mutex::new(HashMap::new())
+    };
+}
+
+
+rental! {
+    mod rent_highlight_cache {
+        use syntect::highlighting::Style;
+        #[rental]
+        pub struct HighlightCacheEntry {
+            string: String,
+            highlighted: Vec<Vec<(Style, &'string str)>>,
+        }
+    }
+}
+
+pub fn with_highlighted_source(file: PathBuf, f: impl FnOnce(&[Vec<(Style, &str)>])) {
+    CACHED_HIGHLIGHTED_FILES
+        .lock()
+        .unwrap()
+        .entry(file.clone())
+        .or_insert_with(|| {
+            if let Ok(src) = std::fs::read_to_string(&file) {
+                Some(HighlightCacheEntry::new(src, |src| {
+                    syntax_highlight(src)
+                }))
+            } else {
+                None
+            }
+        })
+        .as_ref()
+        .unwrap()
+        .rent(|highlighted| f(highlighted));
+}
+
+fn syntax_highlight(src: &str) -> Vec<Vec<(Style, &str)>> {
+    let t = &THEME_SET.themes["Solarized (dark)"];
+    let bg_color = t.settings.background.unwrap_or(Color::WHITE);
+    let mut h = HighlightLines::new(&SYNTAX_SET.find_syntax_by_extension("rs").unwrap().to_owned(), t);
+
+    let mut lines = Vec::new();
+    for line in src.lines() {
+        lines.push(h.highlight(line, &SYNTAX_SET));
+    }
+    lines
+}
+
+pub fn as_24_bit_terminal_escaped(v: &[(Style, &str)], bg: bool) -> String {
+    use std::fmt::Write;
+
+    let mut s: String = String::new();
+    for &(ref style, text) in v.iter() {
+        // 256/6 = 42
+        write!(
+            s,
+            "\x1b[38;5;{}m{}",
+            16u8 + 36 * (style.foreground.r / 42) + 6 * (style.foreground.g / 42) + (style.foreground.b / 42),
+            text
+        ).unwrap();
+    }
+    s.push_str("\x1b[0m");
+    s
+}
