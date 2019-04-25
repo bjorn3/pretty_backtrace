84413f54c94f28f3c200ec8613ac88595548ce3b
bjorn3 <bjorn3@users.noreply.github.com>
2019-04-25 18:43:49 +0200

    Refactor

--- src/display_frame.rs
+++ src/display_frame.rs
@@ -155,165 +155,168 @@ fn print_local(
     indent: usize,
     entry: &gimli::DebuggingInformationEntry<Slice>,
 ) {
-    use gimli::read::Reader;
-
-    let name = if let Some(name) = entry.attr(gimli::DW_AT_name).unwrap() {
-        name.string_value(&dwarf.debug_str).unwrap().to_string().unwrap().into_owned()
-    } else {
-        "<unknown name>".to_string()
-    };
-    println!("{:indent$}name: {}", "", name, indent = indent);
-
-    let ty_offset = if let Some(ty) = entry.attr(gimli::DW_AT_type).unwrap() {
-        match ty.value() {
-            gimli::AttributeValue::UnitRef(unit_offset) => Some(unit_offset),
-            _ => panic!("{:?}", ty.value()),
-        }
-    } else {
-        None
-    };
-
-    let ty_entry = ty_offset.map(|ty_offset| {
-        let mut entries = unit.entries_at_offset(ty_offset).expect("entry");
-        entries.next_entry().unwrap().unwrap();
-        let ty_entry = entries.current().expect("current").clone();
-
-        println!("{:indent$}tag: {}", "", ty_entry.tag().static_string().unwrap(), indent = indent + 4);
-        let name = if let Some(name) = ty_entry.attr(gimli::DW_AT_name).unwrap() {
-            name.string_value(&dwarf.debug_str).unwrap().to_string().unwrap().into_owned()
-        } else {
-            "<unknown name>".to_string()
-        };
-        println!("{:indent$}name: {}", "", name, indent = indent + 4);
-        let mut attrs = ty_entry.attrs();
-        while let Some(attr) = attrs.next().unwrap() {
-            println!("{:indent$}attr {:?} = {:?}", "", attr.name().static_string(), attr.value(), indent = indent + 4);
-            //println!("Attribute value = {:?}", attr.value());
-        }
-
-        ty_entry
-    });
-
-    let exprloc = if let Some(exprloc) = entry.attr(gimli::DW_AT_location).unwrap() {
-        Some(match exprloc.value() {
-            gimli::AttributeValue::Block(data) => gimli::Expression(data),
-            gimli::AttributeValue::Exprloc(exprloc) => exprloc,
-            gimli::AttributeValue::LocationListsRef(loclistref) => {
-                //dwarf.locations(unit, loclistref);
-                println!("warning: unhandled location list");
-                return;
-            },
-            _ => panic!("{:?}", exprloc.value()),
-        })
-    } else {
-        None
-    };
-
-    if let Some(exprloc) = exprloc {
-        let mut eval = exprloc.clone().evaluation(unit.encoding());
-        let mut res = eval.evaluate().unwrap();
-        loop {
-            match res {
-                gimli::EvaluationResult::Complete => {
-                    let result = eval.result();
-                    println!("{:indent$}eval res: {:?}", "", result, indent = indent);
-                    assert!(result.len() == 1);
-                    for piece in result {
-                        use gimli::read::Location::*;
-
-                        assert!(piece.size_in_bits.is_none());
-                        assert!(piece.bit_offset.is_none());
-
-                        match &piece.location {
-                            Empty => println!("{:indent$}piece: empty", "", indent = indent),
-                            Register { register } => {
-                                println!("{:indent$}piece: register={:?}", "", register, indent = indent);
-                            }
-                            Address { address } => {
-                                println!("{:indent$}piece: address={:?}", "", address, indent = indent);
-                            }
-                            Value { value } => {
-                                println!("{:indent$}piece: value={:?}", "", value, indent = indent);
-                            }
-                            Bytes { value } => {
-                                println!("{:indent$}piece: bytes={:?}", "", value, indent = indent);
-                            }
-                            ImplicitPointer { value, byte_offset } => {
-                                println!("{:indent$}piece: implicitptr={:?}+{:?}", "", value, byte_offset, indent = indent);
-                            }
-                        }
-
-                        if let Some(ty_entry) = &ty_entry {
-                            match ty_entry.tag() {
-                                gimli::DW_TAG_base_type => {
-                                    let ty_name = if let Some(name) = ty_entry.attr(gimli::DW_AT_name).unwrap() {
-                                        name.string_value(&dwarf.debug_str).unwrap().to_string().unwrap().into_owned()
-                                    } else {
-                                        "<unknown name>".to_string()
-                                    };
-                                    let encoding = match ty_entry.attr(gimli::DW_AT_encoding).unwrap().unwrap().value() {
-                                        gimli::AttributeValue::Encoding(encoding) => encoding,
-                                        val => panic!("{:?}", val),
-                                    };
-                                    let byte_size = ty_entry.attr(gimli::DW_AT_byte_size).unwrap().unwrap().udata_value().unwrap();
-
-                                    println!("{:indent$}base type {} = {} {}", "", ty_name, encoding.static_string().unwrap(), byte_size, indent = indent);
-
-                                    let bytes = match piece.location {
-                                        Address { address } => {
-                                            unsafe { std::slice::from_raw_parts(address as *const u8, byte_size as usize) }
-                                        }
-                                        _ => panic!("{:?}", piece.location),
-                                    };
-
-                                    println!("{:?}", bytes);
-
-                                    use gimli::Endianity;
-
-                                    match encoding {
-                                        gimli::DW_ATE_signed => println!("{} = {}", name, match bytes.len() {
-                                            1 => bytes[0] as i8 as i64,
-                                            2 => gimli::NativeEndian::default().read_i16(bytes) as i64,
-                                            4 => gimli::NativeEndian::default().read_i32(bytes) as i64,
-                                            8 => gimli::NativeEndian::default().read_i64(bytes) as i64,
-                                            _ => {
-                                                panic!();
-                                            }
-                                        }),
-                                        gimli::DW_ATE_unsigned => println!("{} = {}", name, match bytes.len() {
-                                            1 => bytes[0] as u64,
-                                            2 => gimli::NativeEndian::default().read_u16(bytes) as u64,
-                                            4 => gimli::NativeEndian::default().read_u32(bytes) as u64,
-                                            8 => gimli::NativeEndian::default().read_u64(bytes) as u64,
-                                            _ => {
-                                                panic!();
-                                            }
-                                        }),
-                                        _ => {}
-                                    }
-                                }
-                                _ => {}
-                            }
-                        }
-                    }
-                    break;
-                }
-                // FIXME use DW_AT_frame_base for register
-                gimli::EvaluationResult::RequiresFrameBase => res = eval.resume_with_frame_base(frame.regs[gimli::X86_64::RSP.0].unwrap()).unwrap(),
-                _ => {
-                    println!("{:indent$}eval: {:?}", "", res, indent = indent);
-                    break;
-                }
-            }
-        }
-    }
+    let local_name = entry_name(dwarf, entry);
+    println!("{:indent$}name: {}", "", local_name, indent = indent);
 
     let mut attrs = entry.attrs();
     while let Some(attr) = attrs.next().unwrap() {
         //println!("{:indent$}attr {:?} = ???", "", attr.name().static_string(), indent = indent);
         //println!("Attribute value = {:?}", attr.value());
     }
+
+    let ty_entry = if let Some(ty) = entry.attr(gimli::DW_AT_type).unwrap() {
+        let ty_offset = match ty.value() {
+            gimli::AttributeValue::UnitRef(unit_offset) => unit_offset,
+            _ => panic!("{:?}", ty.value()),
+        };
+
+        let mut entries = unit.entries_at_offset(ty_offset).expect("entry");
+        entries.next_entry().unwrap().unwrap();
+        let ty_entry = entries.current().expect("current").clone();
+
+        println!("{:indent$}tag: {}", "", ty_entry.tag().static_string().unwrap(), indent = indent + 4);
+        println!("{:indent$}name: {}", "", entry_name(dwarf, &ty_entry), indent = indent + 4);
+
+        let mut attrs = ty_entry.attrs();
+        while let Some(attr) = attrs.next().unwrap() {
+            println!("{:indent$}attr {:?} = {:?}", "", attr.name().static_string(), attr.value(), indent = indent + 4);
+        }
+
+        ty_entry
+    } else {
+        println!("warning: missing type for local {}", local_name);
+        return;
+    };
+
+    let exprloc = if let Some(exprloc) = entry.attr(gimli::DW_AT_location).unwrap() {
+        match exprloc.value() {
+            gimli::AttributeValue::Block(data) => gimli::Expression(data),
+            gimli::AttributeValue::Exprloc(exprloc) => exprloc,
+            gimli::AttributeValue::LocationListsRef(_) => {
+                println!("warning: location lists are not yet supported");
+                return;
+            },
+            _ => panic!("{:?}", exprloc.value()),
+        }
+    } else {
+        println!("{} = <unknown>", local_name);
+        return;
+    };
+
+    let eval = exprloc.clone().evaluation(unit.encoding());
+    match evaluate_expression(frame, eval) {
+        Ok(result) => {
+            println!("{:indent$}eval res: {:?}", "", result, indent = indent);
+            assert!(result.len() == 1);
+            for piece in result {
+                use gimli::read::Location::*;
+
+                assert!(piece.size_in_bits.is_none());
+                assert!(piece.bit_offset.is_none());
+
+                match &piece.location {
+                    Empty => println!("{:indent$}piece: empty", "", indent = indent),
+                    Register { register } => {
+                        println!("{:indent$}piece: register={:?}", "", register, indent = indent);
+                    }
+                    Address { address } => {
+                        println!("{:indent$}piece: address={:?}", "", address, indent = indent);
+                    }
+                    Value { value } => {
+                        println!("{:indent$}piece: value={:?}", "", value, indent = indent);
+                    }
+                    Bytes { value } => {
+                        println!("{:indent$}piece: bytes={:?}", "", value, indent = indent);
+                    }
+                    ImplicitPointer { value, byte_offset } => {
+                        println!("{:indent$}piece: implicitptr={:?}+{:?}", "", value, byte_offset, indent = indent);
+                    }
+                }
+
+                match ty_entry.tag() {
+                    gimli::DW_TAG_base_type => {
+                        let ty_name = entry_name(dwarf, &ty_entry);
+                        let encoding = match ty_entry.attr(gimli::DW_AT_encoding).unwrap().unwrap().value() {
+                            gimli::AttributeValue::Encoding(encoding) => encoding,
+                            val => panic!("{:?}", val),
+                        };
+                        let byte_size = ty_entry.attr(gimli::DW_AT_byte_size).unwrap().unwrap().udata_value().unwrap();
+
+                        println!("{:indent$}base type {} = {} {}", "", ty_name, encoding.static_string().unwrap(), byte_size, indent = indent);
+
+                        let bytes = match piece.location {
+                            Address { address } => {
+                                unsafe { std::slice::from_raw_parts(address as *const u8, byte_size as usize) }
+                            }
+                            _ => panic!("{:?}", piece.location),
+                        };
+
+                        println!("{:?}", bytes);
+
+                        use gimli::Endianity;
+
+                        match encoding {
+                            gimli::DW_ATE_signed => println!("{} = {}", local_name, match bytes.len() {
+                                1 => bytes[0] as i8 as i64,
+                                2 => gimli::NativeEndian::default().read_i16(bytes) as i64,
+                                4 => gimli::NativeEndian::default().read_i32(bytes) as i64,
+                                8 => gimli::NativeEndian::default().read_i64(bytes) as i64,
+                                _ => {
+                                    panic!();
+                                }
+                            }),
+                            gimli::DW_ATE_unsigned => println!("{} = {}", local_name, match bytes.len() {
+                                1 => bytes[0] as u64,
+                                2 => gimli::NativeEndian::default().read_u16(bytes) as u64,
+                                4 => gimli::NativeEndian::default().read_u32(bytes) as u64,
+                                8 => gimli::NativeEndian::default().read_u64(bytes) as u64,
+                                _ => {
+                                    panic!();
+                                }
+                            }),
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        Err(res) => {
+            println!("{:indent$}eval: {:?}", "", res, indent = indent);
+            return;
+        }
+    }
+
     println!();
+}
+
+fn entry_name(dwarf: &gimli::Dwarf<Slice>, entry: &gimli::DebuggingInformationEntry<Slice>) -> String {
+    use gimli::read::Reader;
+
+    if let Some(name) = entry.attr(gimli::DW_AT_name).unwrap() {
+        name.string_value(&dwarf.debug_str).unwrap().to_string().unwrap().into_owned()
+    } else {
+        "<unknown name>".to_string()
+    }
+}
+
+fn evaluate_expression(
+    frame: &Frame,
+    mut eval: gimli::Evaluation<Slice>,
+) -> Result<Vec<gimli::Piece<Slice>>, gimli::EvaluationResult<Slice>> {
+    let mut res = eval.evaluate().unwrap();
+    loop {
+        match res {
+            gimli::EvaluationResult::Complete => {
+                return Ok(eval.result());
+            }
+            // FIXME use DW_AT_frame_base for register
+            gimli::EvaluationResult::RequiresFrameBase => res = eval.resume_with_frame_base(frame.regs[gimli::X86_64::RSP.0].unwrap()).unwrap(),
+            err => {
+                return Err(err)
+            }
+        }
+    }
 }
 
 fn find_unit_for_svma(dwarf: &gimli::Dwarf<Slice>, svma: findshlibs::Svma) -> Option<gimli::read::Unit<Slice>> {
