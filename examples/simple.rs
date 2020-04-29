5daeccd2296f7ed4a56b38768164e2403767d141
bjorn3 <bjorn3@users.noreply.github.com>
2020-04-29 16:15:44 +0200

    Switch CI to github actions

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,8 +1,8 @@
 fn main() {
-    pretty_backtrace::setup();
-    please_panic(42);
+    pretty_backtrace::force_setup();
+    assert!(std::panic::catch_unwind(|| please_panic(42)).is_err());
 }
 
-fn please_panic(num: u64) {
+fn please_panic(_num: u64) {
     panic!("Some message");
 }
